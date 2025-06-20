16361b2a300d771748705259e2aaf512af499130
Qabin <qabinkicks@gmail.com>
2025-06-21 01:27:03 +0900

    Frontend ready for Baepo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/OSSW_Ready">
       <App />
     </BrowserRouter>
   </React.StrictMode>
