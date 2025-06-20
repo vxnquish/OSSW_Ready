aeb00de00419832b4a54c95e7d2cb48d8e96eec8
Qabin <qabinkicks@gmail.com>
2025-06-20 22:40:06 +0900

    Front Baepo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'  // BrowserRouter 대신 HashRouter
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 )
